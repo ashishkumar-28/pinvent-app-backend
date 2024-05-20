0ecc4eb6ec0e7ba9dc38f6cd8920e5455026c887
Ashish Kumar <125356595+ashishkumar-28@users.noreply.github.com>
2024-05-20 19:15:34 +0530

    server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));  //helps to handle the data via url
 app.use(bodyParser.json());
 app.use(cors({
-    origin:["http://localhost:3000","https://pinvent-app.vercel.app"],
+    origin:["http://localhost:3000","https://pinvent-app-mu.vercel.app"],
     credentials:true,
 }));
 
